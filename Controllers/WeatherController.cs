cd9fadd23ac05ff36d7ee65f3a4643dde2f8c91e
idesignerse <heberandrade@gmail.com>
2021-04-19 11:07:41 +0200

    Add a partial view
    create a folder and creat a view to connect the new item link.

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -19,7 +19,7 @@ namespace MVC_Sample_1.Controllers
             Weather weather = await GetWeather(town);
             ViewData["Temperature"] = weather.Current.TempC;
             ViewData["Town"] = weather.Location.Name;
-            return View();
+            return PartialView();
         }
 
         private async Task<Weather> GetWeather(string town)
